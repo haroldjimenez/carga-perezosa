83eb5d65a805696c9d74601525adf0796f5420dd
kadelarr <sergio1980>
2017-07-20 13:39:46 -0500

    commit proyecto base

---
+++ src/main/java/com/techandsolve/prueba/cargaperezosa/CargaPerezosaApplication.java
@@ -0,0 +1,12 @@
+package com.techandsolve.prueba.cargaperezosa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargaPerezosaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CargaPerezosaApplication.class, args);
+	}
+}
